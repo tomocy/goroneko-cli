755b209257d0b58eca2ff7b251b87a79b3e19be1
tomocy <tomocy.dev@gmail.com>
2019-07-17 16:12:04 +0900

    add method for clis to run themselves

--- cli.go
+++ cli.go
@@ -1,3 +1,11 @@
 package main
 
-type CLI struct{}
+import "github.com/urfave/cli"
+
+type CLI struct {
+	driver *cli.App
+}
+
+func (c *CLI) Run(args []string) error {
+	return c.driver.Run(args)
+}
